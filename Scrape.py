a2a6ff2559ed63a81afa9eb5c1b5bdee1b3df07a
Radeon992 <Radeon992@gmail.com>
2018-09-17 20:25:26 +0300

    Handling connection error
    try:
    do_smth
    except EXCEPTION_NAME:
    do_smth

--- Scrape.py
+++ Scrape.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import csv
 import sys
 reload(sys)
-sys.setdefaultencoding('utf-8') 
+sys.setdefaultencoding('utf-8')
 
 # Authoriztion with requests with session saving
 # s = requests.session()
@@ -16,20 +16,23 @@ pages = [str(i) for i in range(100,200)] #number of pages you want to pass
 #Iteration through the numbers pf pages and scrape data with BeautifulSoup
 
 for page in pages:
-    html = requests.get('https://statesassembly.gov.je/Pages/Members.aspx?MemberID='+page).text
-    def get_page_data():
-        soup = BeautifulSoup(html, 'lxml') #Soup object
-        name = soup.find('h1').text #Finding Name
-        title = soup.find(class_='gel-layout__item gel-2/3@m gel-1/1@s').find('h2').text #Finding position
+    try:
+        html = requests.get('https://statesassembly.gov.je/Pages/Members.aspx?MemberID='+page).text
+        def get_page_data():
+            soup = BeautifulSoup(html, 'lxml') #Soup object
+            name = soup.find('h1').text #Finding Name
+            title = soup.find(class_='gel-layout__item gel-2/3@m gel-1/1@s').find('h2').text #Finding position
 # Addind to dictionary
-        data = {'name': name,
-                'title': title,
-                }
+            data = {'name': name,
+                    'title': title,
+                    }
 
-        return (data)
+            return (data)
+
+    except requests.exceptions.ConnectionError: #excepting connecting error which I encountered recently
+        continue
 
     data = get_page_data()
-    
     # import to CSV
     with open('Members.csv','a') as output_file:
         writer = csv.writer(output_file, delimiter=';') #using ';' delimeter for Excel
